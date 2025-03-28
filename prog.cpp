3922902ec1b361c91acb5f8bee6e0a5b63de5033
yograj06 <yograj240607@gmail.com>
2025-03-28 22:13:55 +0530

    Modification

--- prog.cpp
+++ prog.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-using namespace std; 
-int main () 
-{
-    cout << "Hello world \n" << endl;
+using namespace std;
+
+int main () {
+    cout << "Hello world," << endl;
+    cout << "I am Yograj Patel." << endl;
     return 0;
 }
