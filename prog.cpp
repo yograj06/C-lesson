8794236b050e0ab5e0711cecc946130ae1c4d74c
yograj06 <yograj240607@gmail.com>
2025-03-29 13:08:42 +0530

    added new code

--- prog.cpp
+++ prog.cpp
@@ -1,8 +1,21 @@
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+//     cout << "Hello world,\n";
+//     cout << "I am Yograj Patel.";
+//     return 0;
+// }
+
+
+// camelCase Notation
+
 #include <iostream>
 using namespace std;
-
-int main () {
-    cout << "Hello world," << endl;
-    cout << "I am Yograj Patel." << endl;
+int main()
+{
+    int marksInMaths = 85;
+    cout<<"The marks of the student in maths is "<<marksInMaths;
     return 0;
-}
+
+} 
---
+++ task.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+int main() 
+{
+    // int y;
+    // printf("Enter year : ");
+    // scanf("%d",&y);
+    // if(y % 400 == 0 || y % 100 != 0 && y % 4 == 0) {          //1
+    //     printf("%d is a leap year",y);
+    // }
+    // else {
+    //     printf("%d is not a leap year",y);
+    // }
+
+
+
+    // int arr[5],i;
+    // for(int i = 0; i < 5; i++) {
+    //     printf("Enter marks subject_%d : ",i + 1);       //2
+    //     scanf("%d", &arr[i]);
+    // }
+    // int sum = 0;
+    // for(int i = 0; i < 5; i++) {
+    //     sum += arr[i];
+    // }
+    // printf("\n");
+    // printf("Total marks : %d\n", sum);    
+    // printf("Average marks : %.2f\n", (float)sum/5.0);
+    // printf("percentage : %.2f%%", (sum/500)*100);
+
+
+
+    // float l;
+    // printf("Enter length in cm : ");                //3
+    // scanf("%f",&l);
+    // printf("Length in m : %.2f\n",l/100);
+    // printf("Length in km : %.5f",l/100000);
+
+
+
+    // int arr[3],i;
+    // for(i = 0; i < 3; i++){
+    //     printf("Enter angle_%d : ", i + 1 );
+    //     scanf("%d", &arr[i]);   
+    // }  
+    // if (arr[i] <= 0) {
+    //     printf("Triangle can not be formed");           //4
+    //     return 0;
+    // } 
+    // int sum = 0;
+    // for(i = 0; i < 3; i++){ 
+    //     sum += arr[i];  
+    // }  
+    // if(sum == 180){
+    //     printf("Triangle can be formed");
+    // }
+    // else {
+    //     printf("Triangle can not be formed");
+    // }
+
+
+
+
+    // int a,b,c;
+    // printf("Enter 3 sides of a triangle : ");
+    // scanf("%d %d %d",&a,&b,&c);
+    // if(a+b<=c || b+c<=a || c+a<=b) {
+    //     printf("Triangle can not be formed");       //5
+    // }
+    // else if(a==b && b==c) {
+    //     printf("Equilateral Triangle");
+    // }
+    // else if(a==b || b==c || c==a) {
+    //     printf("Isosceles Triangle");
+    // }
+    // else {
+    //     printf("Scalene Triangle");
+    // }
+    // return 0;
+}
